6a51f9434ca74a326931519f666a0e8b5e79d770
Violet Leon <52981973+VioletLeon@users.noreply.github.com>
2023-05-16 10:05:05 +0000

    removed 4ever from domains

--- dist/domains.js
+++ dist/domains.js
@@ -11,5 +11,4 @@ exports["default"] = [
     "https://api.estuary.tech/gw/ipfs/:hash",
     "https://ipfs.eth.aragon.network/ipfs/:hash",
     "https://gateway.pinata.cloud/ipfs/:hash",
-    "https://4everland.io/ipfs/:hash",
 ];
